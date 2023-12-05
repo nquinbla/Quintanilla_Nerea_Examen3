643355f8e0f672e25b66a33cc954d40adae2cfe9
nquinbla <nquinbla@myuax.com>
2023-12-05 11:36:11 +0100

    q

--- Estructura básica/Enviroment.cpp
+++ Estructura básica/Enviroment.cpp
@@ -2,8 +2,9 @@
 #include "Enviroment.h"
 
 int main() {
-    Enviroment env;
 
+    Enviroment env;
+    
     env.addVariable("x", 10);
 
     cout << "El valor de x: " << env.getVariable("x") << endl;
