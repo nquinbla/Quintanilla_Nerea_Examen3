73253c4ab5fc1540d76ec4c4d285ee6e10f1f4ce
nquinbla <nquinbla@myuax.com>
2023-12-05 11:42:26 +0100

    error

--- Insertar Símbolo/main.cpp
+++ Insertar Símbolo/main.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-class Enviroment ¨{
+class Enviroment {
 public:
     Enviroment();
     void set(const string &symbol, int value);
@@ -25,3 +25,11 @@ int Enviroment::get(const string &symbol) const {
     return variables.at(symbol);
 }
 
+void Enviroment::insert(const string &symbol, int value) {
+    if (variables.find(symbol) == variables.end()) {
+        variables[symbol] = value;
+    }
+    else {
+        throw invalid_argument("La variable ya existe");
+    }
+}
